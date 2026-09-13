74c6ef02c1829fca630f2c7ae9b37efaf3bd6d7e
Vishal Gopal Ojha <vishal@chaoscraftlabs.com>
2026-09-14 03:55:15 +0530

    constrain Linux widget orb image size

--- aurora/panel.py
+++ aurora/panel.py
@@ -38,7 +38,7 @@ class KimButton(Gtk.Window):
         self.set_skip_taskbar_hint(False)
         self.set_keep_above(True)
         self.set_resizable(False)
-        self.set_default_size(180, 50)
+        self.set_default_size(150, 42)
         self.set_position(Gtk.WindowPosition.NONE)
         screen = Gdk.Screen.get_default()
         if screen:
@@ -68,8 +68,8 @@ class KimButton(Gtk.Window):
             background-color: #343bff;
             border: 2px solid #a5afff;
             border-radius: 14px;
-            min-width: 34px;
-            min-height: 34px;
+            min-width: 28px;
+            min-height: 28px;
         }
         #kim-orb.active {
             background-color: #693cff;
@@ -97,20 +97,21 @@ class KimButton(Gtk.Window):
         self.drag_handle = Gtk.EventBox()
         self.drag_handle.add(Gtk.Label(label="⠿"))
         self.drag_handle.set_tooltip_text("Drag Kim anywhere")
-        self.drag_handle.set_size_request(8, 36)
+        self.drag_handle.set_size_request(6, 30)
         self.drag_handle.connect("button-press-event", self._start_drag)
         self.orb_row.pack_start(self.drag_handle, False, False, 0)
 
         self.shell = Gtk.EventBox()
         self.shell.set_name("kim-orb-shell")
-        self.shell.set_size_request(40, 40)
+        self.shell.set_size_request(34, 34)
 
         self.btn = Gtk.EventBox()
         self.btn.set_name("kim-orb")
         self.orb_image = Gtk.Image.new_from_file(str(ORB_ASSET))
-        self.orb_image.set_pixel_size(34)
+        self.orb_image.set_pixel_size(28)
+        self.orb_image.set_size_request(28, 28)
         self.btn.add(self.orb_image)
-        self.btn.set_size_request(36, 36)
+        self.btn.set_size_request(30, 30)
         self.shell.add(self.btn)
         self.orb_row.pack_start(self.shell, True, True, 0)
         self.btn.connect("button-press-event", self.on_clicked)
@@ -302,7 +303,7 @@ class KimButton(Gtk.Window):
             self.show_all()
             return
         geo = monitor.get_geometry()
-        w, h = 180, 50
+        w, h = 150, 42
         self.move(geo.x + (geo.width - w) // 2, geo.y + 38)
         self.show_all()
 
